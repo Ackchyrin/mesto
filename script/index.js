cc766054ce04ec1d6feb8e3c3fcfafc87ebc549e
Ackchyrin <86160552+Ackchyrin@users.noreply.github.com>
2021-10-17 15:26:46 +0500

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,70 @@
+/* закрытие и открытие popup */
+const popupElementProfile = document.querySelector('.popup');
+const popupCloseButtonElement = popupElementProfile.querySelector('.popup__close');
+const popupOpenButtonElement = document.querySelector('.profile__info-button');
+
+const openPopup = function () {
+  popupElementProfile.classList.add('popup__open');
+};
+
+const closePopup = function () {
+  popupElementProfile.classList.remove('popup__open');
+};
+
+popupCloseButtonElement.addEventListener('click', closePopup);
+
+const closePopupByClickOverlay = function (event) {
+  if (event.target !== event.currentTarget) {
+    return;
+  }
+
+  closePopup();
+};
+
+popupElementProfile.addEventListener('click', closePopupByClickOverlay);
+
+/* Изменение данных профиля в popup */
+
+const profileName = document.querySelector('.profile__info-name');
+const profileEmployment = document.querySelector('.profile__info-about');
+const nameInput = popupElementProfile.querySelector('.popup__name');
+const employmentInput = popupElementProfile.querySelector('.popup__about');
+
+function popupValue () {
+  nameInput.value = profileName.textContent;
+  employmentInput.value = profileEmployment.textContent;
+
+  openPopup();
+};
+
+popupOpenButtonElement.addEventListener('click', popupValue);
+
+/* Сохранение изменений в профиле */
+
+const buttonSavePopupChanges = popupElementProfile.querySelector('.popup__save')
+
+const savePopupChanges = function() {
+  popupElementProfile.classList.remove('popup__open');
+};
+
+buttonSavePopupChanges.addEventListener('click',saveNewInformation);
+
+function saveNewInformation (event) {
+  event.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileEmployment.textContent = employmentInput.value;
+
+  savePopupChanges();
+};
+
+/*Постановка лайка на фотокарточку */
+
+const elementLike = document.querySelector('.element__like');
+
+function pushLike() {
+  elementLike.classList.toggle('element__like-pressed');
+};
+
+elementLike.addEventListener('click', pushLike);
+
