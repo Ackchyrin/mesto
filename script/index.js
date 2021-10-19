4567895317a3e173c00f97a6cd914147520c660c
Ackchyrin <86160552+Ackchyrin@users.noreply.github.com>
2021-10-19 19:48:21 +0500

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -4,21 +4,21 @@ const popupCloseButtonElement = popupElementProfile.querySelector('.popup__close
 const popupOpenButtonElement = document.querySelector('.profile__info-button');
 const profileName = document.querySelector('.profile__info-name');
 const profileEmployment = document.querySelector('.profile__info-about');
-const nameInput = popupElementProfile.querySelector('.popup__input-name');
-const employmentInput = popupElementProfile.querySelector('.popup__input-about');
+const nameInput = popupElementProfile.querySelector('.popup__input_name');
+const employmentInput = popupElementProfile.querySelector('.popup__input_about');
 const buttonSavePopupChanges = popupElementProfile.querySelector('.popup__save')
 
 function valuePopup () {
   nameInput.value = profileName.textContent;
   employmentInput.value = profileEmployment.textContent;
 
-  popupElementProfile.classList.add('popup-open');
+  popupElementProfile.classList.add('popup_open');
 };
 
 popupOpenButtonElement.addEventListener('click',valuePopup);
 
 const closePopup = function () {
-  popupElementProfile.classList.remove('popup-open');
+  popupElementProfile.classList.remove('popup_open');
 };
 
 popupCloseButtonElement.addEventListener('click', closePopup);
